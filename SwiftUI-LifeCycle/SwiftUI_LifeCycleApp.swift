dae0955fc3506ea36cd31bde9c97be71c3bbece5
goutham <gouthamshine@gmail.com>
2022-07-03 22:44:08 +0530

    using scenePhase environment variable

--- SwiftUI-LifeCycle/SwiftUI_LifeCycleApp.swift
+++ SwiftUI-LifeCycle/SwiftUI_LifeCycleApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct SwiftUI_LifeCycleApp: App {
+    @Environment(\.scenePhase) var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                print("Scene - Active")
+            case .inactive:
+                print("Scene - Inactive")
+            case .background:
+                print("Scene - Background")
+            @unknown default:
+                print("Scene - Unknown")
+            }
         }
     }
 }
