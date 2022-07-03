e2c19e8783b0cf14946157a671335dace4f5b29f
goutham <gouthamshine@gmail.com>
2022-07-03 22:28:29 +0530

    Initial Commit

---
+++ SwiftUI-LifeCycle/SwiftUI_LifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LifeCycleApp.swift
+//  SwiftUI-LifeCycle
+//
+//  Created by Goutham S on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
